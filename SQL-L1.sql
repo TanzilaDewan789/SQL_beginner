20ce23d136bd2ceb6114996051d15b5bf0d10d50
TanzilaDewanSamira <tanzila.dewan789@gmail.com>
2026-04-04 03:28:13 +0600

    database related quries

--- SQL-L1.sql
+++ SQL-L1.sql
@@ -18,3 +18,27 @@ VALUES
 
 #show the entire table
 SELECT * FROM student;
+
+#1.Create a database & check if its exist or not (University_game)
+#2.Show all the tables and all the databases
+#3.DROP the database if exist
+
+CREATE DATABASE IF NOT EXISTS University_game;
+USE University_game;
+
+CREATE TABLE team(
+id INT PRIMARY KEY,
+age INT NOT NULL,
+interest VARCHAR (50)
+);
+
+INSERT INTO team(id, age, interest)
+VALUES
+(1, 18, 'FOOTBALL'),
+(2, 17, 'RUGBY');
+
+SELECT * FROM team;
+
+SHOW DATABASES;
+SHOW TABLES;
+DROP DATABASE IF EXISTS University_game;
