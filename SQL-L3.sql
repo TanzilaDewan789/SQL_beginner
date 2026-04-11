6121c9fa98da586845dde58d04627672239d6153
TanzilaDewanSamira <tanzila.dewan789@gmail.com>
2026-04-11 16:06:12 +0600

    on and off cascade

--- SQL-L3.sql
+++ SQL-L3.sql
@@ -31,3 +31,37 @@ VALUES
 SELECT * FROM dept;
 SELECT * FROM teachers_info;
 
+#CASCADING FOREIGN KEY
+#1.ON UPDATE CASCADE:
+CREATE TABLE new_dept( 
+  id INT PRIMARY KEY,
+  name VARCHAR(50)
+);
+
+INSERT INTO new_dept (id, name)
+ VALUES
+(102, 'it'),
+(200, 'arts');
+
+CREATE TABLE new_teacher(
+  id INT PRIMARY KEY,
+  name VARCHAR(50),
+  course VARCHAR (50),
+  dept_id INT,
+  FOREIGN KEY (dept_id) REFERENCES new_dept(id)
+  ON UPDATE CASCADE
+  ON DELETE CASCADE
+);
+INSERT INTO new_teacher (id, name,course, dept_id)
+ VALUES 
+ (1, 'adam', 'it', 102), 
+ (2, 'bob', 'arts', 200);
+
+UPDATE new_dept
+SET id = 103 , name ='geo'
+WHERE id = 102 AND name= 'it';
+
+SELECT * FROM new_dept;
+SELECT * FROM  new_teacher ;
+
+DELETE FROM new_dept WHERE id = 200;
